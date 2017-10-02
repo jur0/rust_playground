ad0be1d933cd326f3260d929042e40856f94fc41
Juraj Hlista <jurajhlista@gmail.com>
2017-10-02 18:16:06 +0000

    Add 18_concurrency

---
+++ 18_concurrency/share_immutable/src/main.rs
@@ -0,0 +1,26 @@
+use std::thread;
+use std::sync::Arc;
+
+static NTHREADS: i32 = 10;
+
+fn main() {
+    // Make a vector to hold the children which are spawned.
+    let mut children = vec![];
+    let shared_data: Arc<Vec<i32>> = Arc::new(vec![1, 2, 3]);
+
+    for i in 0..NTHREADS {
+        let shared_data_for_child: Arc<Vec<i32>> = shared_data.clone();
+
+        // Spin up another thread.
+        children.push(thread::spawn(move || {
+            println!("this is thread number {} with shared data: {:?}",
+                i, &shared_data_for_child);
+            i
+        }));
+    }
+
+    for child in children {
+        // Wait for the thread to finish. Returns a result.
+        println!("join result {:?}", child.join());
+    }
+}
